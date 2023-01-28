3ae51a681d1fb201e989a99423e1bf32ba9cae0b
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2023-01-28 11:45:39 -0600

    add chart

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const nameData = require('./controller')
-console.log(nameData('John'))
+const ctx = document.getElementById('myChart');
 
-const getData = async () => {
-  const data = await nameData('John')
-  console.log('aqui data', data)
-}
+const data = [
+  { year: 2010, count: 10 },
+  { year: 2011, count: 20 },
+  { year: 2012, count: 15 },
+  { year: 2013, count: 25 },
+  { year: 2014, count: 22 },
+  { year: 2015, count: 30 },
+  { year: 2016, count: 28 },
+];
 
-nameData('John')
-  .then((response) => console.log('response', response))
-
-getData()
+new Chart(ctx, {
+  type: 'scatter',
+  data: {
+    labels: data.map(row => row.year),
+    datasets: [
+      {
+        label: 'Acquisitions by year',
+        data: data.map(row => row.count)
+      }
+    ]
+  }
+});
